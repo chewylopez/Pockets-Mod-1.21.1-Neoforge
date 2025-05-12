4e3bce3e2b089e97b935475cb02a2b50976c7885
chewylopez <47454950+chewylopez@users.noreply.github.com>
2025-05-11 22:20:19 -0400

    curios api integration

--- src/main/java/com/chewylopez/pocketsmod/PocketsMod.java
+++ src/main/java/com/chewylopez/pocketsmod/PocketsMod.java
@@ -1,7 +1,6 @@
 package com.chewylopez.pocketsmod;
 
 import com.chewylopez.pocketsmod.enchantment.ModEnchantmentEffects;
-import com.chewylopez.pocketsmod.player.client.ModMenuTypes;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -9,18 +8,11 @@ import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(PocketsMod.MODID)
@@ -36,8 +28,6 @@ public class PocketsMod
         NeoForge.EVENT_BUS.register(this);
 
         ModEnchantmentEffects.register(modEventBus);
-
-        ModMenuTypes.register(modEventBus);
 
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
