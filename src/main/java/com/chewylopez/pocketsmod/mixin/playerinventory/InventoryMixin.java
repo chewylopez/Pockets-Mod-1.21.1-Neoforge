0dbea555b7e4e39c5d8a96740a0a0fa4608391a9
chewylopez <pabloantoniolopez@icloud.com>
2026-05-17 02:02:58 -0400

    pockets inventory compartments added

--- src/main/java/com/chewylopez/pocketsmod/mixin/playerinventory/InventoryMixin.java
+++ src/main/java/com/chewylopez/pocketsmod/mixin/playerinventory/InventoryMixin.java
@@ -1,79 +1,44 @@
 package com.chewylopez.pocketsmod.mixin.playerinventory;
 
-import com.chewylopez.pocketsmod.mixin.playerinventory.interfaces.IInventoryPartition;
 import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Mixin({Inventory.class})
-public abstract class InventoryMixin implements IInventoryPartition {
+@Mixin(value = Inventory.class, priority = 1)
+public class InventoryMixin {
 
     @Unique
-    private static int VANILLA_INVENTORY_SIZE = 36;
+    private static int VANILLA_INVENTORY_SIZE = 45;
     @Unique
     private static int EXTRA_SLOTS_SIZE = 9;
     @Unique
     private static int POCKETS_SIZE = 16;
 
+    @Mutable @Shadow @Final
+    public NonNullList<ItemStack> items;
 
-    @Shadow @Final
-    private List<NonNullList<ItemStack>> compartments;
-
-    @Mutable @Unique @Final
-    private NonNullList<ItemStack> helmet_pocket = NonNullList.withSize(4, ItemStack.EMPTY);
-    @Mutable @Unique @Final
-    private NonNullList<ItemStack> chest_pocket = NonNullList.withSize(4, ItemStack.EMPTY);
-    @Mutable @Unique @Final
-    private NonNullList<ItemStack> legs_pocket = NonNullList.withSize(4, ItemStack.EMPTY);
-    @Mutable @Unique @Final
-    private NonNullList<ItemStack> boots_pocket = NonNullList.withSize(4, ItemStack.EMPTY);
-
-
-    @Override
-    public NonNullList<ItemStack> pockets_Mod_1_21_1_Neoforge$getHelmetInventoryPartition() {
-        return helmet_pocket;
-    }
-    @Override
-    public NonNullList<ItemStack> pockets_Mod_1_21_1_Neoforge$getChestInventoryPartition() {
-        return chest_pocket;
-    }
-    @Override
-    public NonNullList<ItemStack> pockets_Mod_1_21_1_Neoforge$getLegsInventoryPartition() {
-        return legs_pocket;
-    }
-    @Override
-    public NonNullList<ItemStack> pockets_Mod_1_21_1_Neoforge$createBootsInventoryPartition() {
-        return boots_pocket;
-    }
-
-    @Inject(method = {"<init>"},at =  @At("TAIL"))
-    private void addToCompartments(Player player, CallbackInfo ci) {
-        this.compartments.add(this.helmet_pocket);
-        this.compartments.add(this.chest_pocket);
-        this.compartments.add(this.legs_pocket);
-        this.compartments.add(this.boots_pocket);
-    }
-
-
-    @ModifyArg(method = {"<init>"}, index = 0, at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/core/NonNullList;withSize(ILjava/lang/Object;)Lnet/minecraft/core/NonNullList;"))
-    private int modifyInvSize(int size) {
-        return VANILLA_INVENTORY_SIZE + EXTRA_SLOTS_SIZE + POCKETS_SIZE; //61
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void changeItemsSize(CallbackInfo ci) {
+        this.items = NonNullList.withSize(45, ItemStack.EMPTY);
     }
     @ModifyConstant(method = "getSlotWithRemainingSpace", constant = @Constant(intValue = 40, ordinal = 0))
     private int changeInventoryReferenceSize(int constant) {
-        return constant + EXTRA_SLOTS_SIZE + POCKETS_SIZE;
+        return constant + 9;
     }
     @ModifyConstant(method = "getSlotWithRemainingSpace", constant = @Constant(intValue = 40, ordinal = 1))
     private int changeInventoryReferenceSize2(int constant) {
-        return constant + EXTRA_SLOTS_SIZE + POCKETS_SIZE;
+        return constant + 9;
     }
-
 
 }
 
---
+++ src/main/java/com/chewylopez/pocketsmod/mixin/playerinventory/InventoryMixinPockets.java
@@ -0,0 +1,147 @@
+package com.chewylopez.pocketsmod.mixin.playerinventory;
+
+import com.chewylopez.pocketsmod.InventoryInterface.IInventoryPartition;
+import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mixin(value = Inventory.class, priority = 2)
+public class InventoryMixinPockets implements IInventoryPartition {
+
+    @Mutable @Shadow @Final
+    private List<NonNullList<ItemStack>> compartments;
+
+    @Shadow @Final
+    public Player player;
+    @Final @Shadow
+    public NonNullList<ItemStack> items;
+    @Final @Shadow
+    public NonNullList<ItemStack> armor;
+    @Final @Shadow
+    public NonNullList<ItemStack> offhand;
+
+    @Unique
+    private NonNullList<ItemStack> helmet_pocket;
+    @Unique
+    private NonNullList<ItemStack> chest_pocket;
+    @Unique
+    private NonNullList<ItemStack> legs_pocket;
+    @Unique
+    private NonNullList<ItemStack> boots_pocket;
+
+    @Inject(method = {"<init>"},at =  @At("TAIL"))
+    private void addToCompartments(CallbackInfo ci) {
+
+        this.helmet_pocket = NonNullList.withSize(4, ItemStack.EMPTY);
+        this.chest_pocket = NonNullList.withSize(4, ItemStack.EMPTY);
+        this.legs_pocket = NonNullList.withSize(4, ItemStack.EMPTY);
+        this.boots_pocket = NonNullList.withSize(4, ItemStack.EMPTY);
+
+        List<NonNullList<ItemStack>> mutable = new ArrayList<>();
+
+        mutable.add(this.items);
+        mutable.add(this.armor);
+        mutable.add(this.offhand);
+
+        mutable.add(this.helmet_pocket);
+        mutable.add(this.chest_pocket);
+        mutable.add(this.legs_pocket);
+        mutable.add(this.boots_pocket);
+
+        this.compartments = mutable;
+    }
+
+    @Override
+    public NonNullList<ItemStack> pockets_Mod_1_21_1_Neoforge$getHelmetInventoryPartition() {
+        return helmet_pocket;
+    }
+
+    @Override
+    public NonNullList<ItemStack> pockets_Mod_1_21_1_Neoforge$getChestInventoryPartition() {
+        return chest_pocket;
+    }
+
+    @Override
+    public NonNullList<ItemStack> pockets_Mod_1_21_1_Neoforge$getLegsInventoryPartition() {
+        return legs_pocket;
+    }
+
+    @Override
+    public NonNullList<ItemStack> pockets_Mod_1_21_1_Neoforge$createBootsInventoryPartition() {
+        return boots_pocket;
+    }
+
+    @Inject(method = "save", at = @At("RETURN"))
+    private void mymod$save(ListTag tag, CallbackInfoReturnable<ListTag> cir) {
+
+        for (int i = 0; i < 4; i++) {
+            ItemStack stack = helmet_pocket.get(i);
+            if (!stack.isEmpty()) {
+                CompoundTag compound = new CompoundTag();
+                compound.putByte("Slot", (byte)(200 + i));
+                tag.add(stack.save(this.player.registryAccess(), compound));
+            }
+        }
+
+        for (int i = 0; i < 4; i++) {
+            ItemStack stack = chest_pocket.get(i);
+            if (!stack.isEmpty()) {
+                CompoundTag compound = new CompoundTag();
+                compound.putByte("Slot", (byte)(210 + i));
+                tag.add(stack.save(this.player.registryAccess(), compound));
+            }
+        }
+
+        for (int i = 0; i < 4; i++) {
+            ItemStack stack = legs_pocket.get(i);
+            if (!stack.isEmpty()) {
+                CompoundTag compound = new CompoundTag();
+                compound.putByte("Slot", (byte)(220 + i));
+                tag.add(stack.save(this.player.registryAccess(), compound));
+            }
+        }
+
+        for (int i = 0; i < 4; i++) {
+            ItemStack stack = boots_pocket.get(i);
+            if (!stack.isEmpty()) {
+                CompoundTag compound = new CompoundTag();
+                compound.putByte("Slot", (byte)(230 + i));
+                tag.add(stack.save(this.player.registryAccess(), compound));
+            }
+        }
+
+    }
+
+    @Inject(method = "load", at = @At("HEAD"))
+    private void mymod$load(ListTag tag, CallbackInfo ci) {
+        for (int i = 0; i < tag.size(); i++) {
+            CompoundTag compound = tag.getCompound(i);
+            int slot = compound.getByte("Slot") & 255;
+            if (slot >= 200 && slot < 200 + helmet_pocket.size()) {
+                helmet_pocket.set(slot - 200, ItemStack.parseOptional(this.player.registryAccess(), compound));
+            }
+            else if (slot >= 210 && slot < 210 + chest_pocket.size()) {
+                chest_pocket.set(slot - 210, ItemStack.parseOptional(this.player.registryAccess(), compound));
+            }
+            else if (slot >= 220 && slot < 220 + legs_pocket.size()) {
+                legs_pocket.set(slot - 220, ItemStack.parseOptional(this.player.registryAccess(), compound));
+            }
+            else if (slot >= 230 && slot < 230 + boots_pocket.size()) {
+                boots_pocket.set(slot - 230, ItemStack.parseOptional(this.player.registryAccess(), compound));
+            }
+        }
+    }
+
+
+}
